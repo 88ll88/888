d6513ccb2581a4a8556be30340d0162415d42895
88ll88 <lumingxian666@gmail.com>
2026-06-29 06:04:47 +0800

    Update zzz.js

--- zzz.js
+++ zzz.js
@@ -11,3 +11,21 @@ var zzz = ddd.map((item, index) => {
     }
     return [dateStr, news, originalOrder, currentId];
 });
+
+var DateStr = (function(){
+  return new Intl.DateTimeFormat('zh-TW', {
+    year: 'numeric', month: '2-digit', day: '2-digit'
+  }).format(new Date()).replace(/\//g, '-');
+})();
+
+var WeekStr = (function(){
+  // 'narrow' 會直接輸出 "一"、"二"、"日"
+  const w = new Intl.DateTimeFormat('zh-TW', { weekday: 'narrow' }).format(new Date());
+  return `(${w})`;
+})();
+
+var TimeStr = (function(){
+  return new Intl.DateTimeFormat('zh-TW', {
+    hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h23'
+  }).format(new Date());
+})();
